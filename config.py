b64f27f2ca298963def47edf3c51a6e1356ebdc8
NovikovDanil <85548292+NovikovDanil@users.noreply.github.com>
2024-03-22 22:51:56 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+
+TOKEN = '7061421511:AAHtRtJzZasC47lTAHb6Gw8hwIO8cczMpcM'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from config import keys
+class ConvertionException(Exception):
+    pass
+
+class CryptoConvert():
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты{base}.')
+        try:
+            quote_tiker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {quote}")
+        try:
+            base_tiker = keys[base]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {base}")
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f"Не удалось обработать количество {amount}")
+
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_tiker}&tsyms={base_tiker}")
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base
